1782511fbf1054b3a9ec80e4edd43fd6af552ecb
Manikandan Sundararajan <tsmanikandan@protonmail.com>
2020-05-04 01:34:22 -0500

    use rainbowAnimation keyframe for extra pizzaz

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import Layout from './../components/Layout';
+import Layout, { rainbowAnimation } from './../components/Layout';
 import face from './../../static/face.png';
 
 const StyledContainer = styled.div`
@@ -26,26 +26,20 @@ const StyledImg = styled.img`
   }
 `;
 
+const StyledH4 = styled.h4`
+  animation: ${rainbowAnimation(2)} 4s ease 2s infinite reverse;
+  font-style: italic;
+  text-shadow: 2px 2px 0px #ff0000;
+`;
+
 const MainFace = () => {
-  const randColors = [
-    '#ff124f', // Bright red
-    '#ff00a0', // Bright pink
-    '#fe75fe', // Light pink
-    '#1afe49', // Bright Green
-    '#73fffe' // Light corral
-  ];
-  const curColor = randColors[Math.floor(Math.random() * randColors.length)];
   return (
     <div style={{ textAlign: 'center' }}>
       <StyledImg src={face} alt="My face" />
       <h3>
         <b>Manikandan Sundararajan</b>
       </h3>
-      <h4
-        style={{ textShadow: '2px 2px 0px ' + curColor, fontStyle: 'italic' }}
-      >
-        Software Dev | Keyboard Nerd
-      </h4>
+      <StyledH4>Software Dev | Keyboard Nerd</StyledH4>
       <blockquote style={{ fontStyle: 'italic' }}>
         "Some one once told me that I did a good job. <br />
         I've been chasing that high ever since"
